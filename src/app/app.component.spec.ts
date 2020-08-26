1ec2c707615378626a54484a5330e723507dffbc
andiwoedwin <andiwoedwin@gmail.com>
2020-08-26 21:58:47 +0300

    add author

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,7 +26,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Goals');
   });
 
-  it('should render title', () => {
+  it('should render title', () => {z
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
